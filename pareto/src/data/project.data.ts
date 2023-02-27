59e6aaf6c44688d768cbd242275e14a8b3731a17
corno <corno.astn.a@schraverus.com>
2023-02-27 15:43:24 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -9,7 +9,7 @@ import { $ as api } from "./api.data"
 export const $: mproject.T.Project<pd.SourceLocation> = {
     'author': "Corno",
     'description': "functions to convert JSON data to Pareto types",
-    'license': "ISC",
+    'license': "TBD",
     
     'dependencies': d({
         "glo-pareto-common": {},
